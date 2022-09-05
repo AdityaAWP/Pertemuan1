98cde4d3b1a5c199973001e1e0b001f06317568f
AdityaAWP <60000844+AdityaAWP@users.noreply.github.com>
2022-09-05 18:20:40 +0700

    First Commit

---
+++ Pertemuan_1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama: Aditya Wulan Pratama" << endl;
+    cout << "NIM: A11.2022.14148" << endl;
+    cout << "Alamat: Pati, Jawa Tengah" << endl;
+    cout << "Asal Sekolah: SMK Raden Umar Said Kudus" << endl;
+    return 0;
+}
