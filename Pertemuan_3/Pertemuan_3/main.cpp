d50c23f626fe134ea91675b7a5e9a2912efeafb7
AdityaAWP <111202214148@mhs.dinus.ac.id>
2022-09-12 15:41:13 +0700

    Commit

---
+++ Pertemuan_3/Pertemuan_3/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age=18;
+    float height=1.81;
+    char ub='L';
+    string name="Aditya";
+
+    cout << "Umur Saya: " << age;
+    cout << "\nTinggi Saya: " << height;
+    cout << "\nUkuran Baju: " << ub;
+    cout << "\nNama Saya: "<< name;
+
+    return 0;
+}
